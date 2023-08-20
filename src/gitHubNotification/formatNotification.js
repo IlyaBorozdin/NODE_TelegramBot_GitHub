470807a7cf764c07615d3cfd8b1f791ba153428a
IlyaBorozdin <borozdin.ilshat2004@gmail.com>
2023-08-20 15:59:48 +0300

    функционал готов

--- src/gitHubNotification/formatNotification.js
+++ src/gitHubNotification/formatNotification.js
@@ -1,108 +1,77 @@
 function formatNotification(data) {
-    if (data.zen) {
-        return formatWebhookConnected(data);
-    } else if (data.ref) {
-        return formatCommit(data);
-    } else if (data.issue && data.action === 'opened') {
-        return formatIssue(data);
-    } else if (data.issue && data.action === 'created') {
-        return formatIssueComment(data);
-    } else if (data.issue && data.action === 'locked') {
-        return formatLockedIssue(data);
-    } else if (data.issue && data.action === 'unlocked') {
-        return formatUnlockedIssue(data);
-    } else if (data.issue && data.action === 'deleted') {
-        return formatDeletedIssue(data);
-    } else {
-        return 'Received an unknown notification format';
+    if (data.ref) {
+        return commit(data);
+    } else if (data.hook_id) {
+        if (data.action === 'deleted') {
+            return webhookDeleted(data);
+        } else if (data.zen) {
+            return webhookConnected(data);
+        }
+    } else if (data.issue) {
+        if (data.action === 'opened') {
+            return issueOpened(data);
+        } else if (data.action === 'created') {
+            return issueCreared(data);
+        } else if (data.action === 'locked') {
+            return issueLocked(data);
+        } else if (data.action === 'unlocked') {
+            return issueUnlocked(data);
+        } else if (data.action === 'deleted') {
+            return issueDeleted(data);
+        }
     }
+    return 'Received an unknown notification format';
 }
 
-function formatWebhookConnected(data) {
-    return `"${data.zen}"\n` +
-        `${data.sender.login}'s webhook in repository ${data.repository.name} connected successfully!\n` +
-        `More details: ${data.repository.html_url}`;
-}
+const webhookConnected = (data) =>
+    `"${data.zen}"\n` +
+    `${data.sender.login}'s webhook in repository ${data.repository.name} connected successfully!\n` +
+    `More details: ${data.repository.html_url}`;
 
-function formatGitHubEvent(data) {
-    switch (data.event) {
-        case 'pull_request':
-            return formatPullRequestEvent(data);
-        default:
-            return `Received an unknown event type: ${data.event}`;
-    }
-}
+const webhookDeleted = (data) =>
+    `Webhook is deleted in repository ${data.repository.name}\n` +
+    `Author: ${data.sender.login}\n` +
+    `More details: ${data.repository.html_url}`;
 
-function formatPullRequestEvent(data) {
-    const pullRequest = data.payload.pull_request;
-    return `New Pull Request in repository ${data.repository.name}\n` +
-        `Pull Request: ${pullRequest.title}\n` +
-        `Description: ${pullRequest.body}\n` +
-        `Author: ${pullRequest.user.login}\n` +
-        `More details: ${pullRequest.html_url}`;
-}
+const commit = (data) =>
+    `New Commit in repository ${data.repository.name}\n` +
+    `Message: ${data.head_commit.message}\n` +
+    `Author: ${data.head_commit.author.username}\n` +
+    `More details: ${data.repository.html_url}`;
 
-function formatGitHubAction(data) {
-    switch (data.action) {
-        case 'deleted':
-            return formatDeletedAction(data);
-        default:
-            return `Received an unknown action type: ${data.action}`;
-    }
-}
+const issueOpened = (data) =>
+    `New Issue in repository ${data.repository.name}\n` +
+    `Issue: ${data.issue.title}\n` +
+    `Description: ${data.issue.body}\n` +
+    `Author: ${data.issue.user.login}\n` +
+    `More details: ${data.issue.html_url}`;
 
-function formatDeletedAction(data) {
-    return `Webhook is deleted in repository ${data.repository.name}\n` +
-        `Author: ${data.sender.login}\n` +
-        `More details: ${data.repository.html_url}`;
-}
+const issueCreared = (data) =>
+    `New comment to Issue in repository ${data.repository.name}\n` +
+    `Issue: ${data.issue.title}\n` +
+    `Comment: ${data.comment.body}\n` +
+    `Author: ${data.comment.user.login}\n` +
+    `More details: ${data.comment.html_url}`;
 
-function formatCommit(data) {
-    const commit = data.head_commit;
-    return `New Commit in repository ${data.repository.name}\n` +
-        `Message: ${commit.message}\n` +
-        `Author: ${commit.author.username}\n` +
-        `More details: ${data.repository.html_url}`;
-}
+const issueLocked = (data) =>
+    `The issue is locked in repository ${data.repository.name}\n` +
+    `Issue: ${data.issue.title}\n` +
+    `Description: ${data.issue.body}\n` +
+    `Author: ${data.issue.user.login}\n` +
+    `More details: ${data.issue.html_url}`;
 
-function formatIssue(data) {
-    return `New Issue in repository ${data.repository.name}\n` +
-        `Issue: ${data.issue.title}\n` +
-        `Description: ${data.issue.body}\n` +
-        `Author: ${data.issue.user.login}\n` +
-        `More details: ${data.issue.html_url}`;
-}
+const issueUnlocked = (data) =>
+    `The issue is unlocked in repository ${data.repository.name}\n` +
+    `Issue: ${data.issue.title}\n` +
+    `Description: ${data.issue.body}\n` +
+    `Author: ${data.issue.user.login}\n` +
+    `More details: ${data.issue.html_url}`;
 
-function formatIssueComment(data) {
-    return `New comment to Issue in repository ${data.repository.name}\n` +
-        `Issue: ${data.issue.title}\n` +
-        `Comment: ${data.comment.body}\n` +
-        `Author: ${data.comment.user.login}\n` +
-        `More details: ${data.comment.html_url}`;
-}
-
-function formatLockedIssue(data) {
-    return `The issue is locked in repository ${data.repository.name}\n` +
-        `Issue: ${data.issue.title}\n` +
-        `Description: ${data.issue.body}\n` +
-        `Author: ${data.issue.user.login}\n` +
-        `More details: ${data.issue.html_url}`;
-}
-
-function formatUnlockedIssue(data) {
-    return `The issue is unlocked in repository ${data.repository.name}\n` +
-        `Issue: ${data.issue.title}\n` +
-        `Description: ${data.issue.body}\n` +
-        `Author: ${data.issue.user.login}\n` +
-        `More details: ${data.issue.html_url}`;
-}
-
-function formatDeletedIssue(data) {
-    return `The issue is deleted in repository ${data.repository.name}\n` +
-        `Issue: ${data.issue.title}\n` +
-        `Description: ${data.issue.body}\n` +
-        `Author: ${data.issue.user.login}\n` +
-        `More details: ${data.issue.html_url}`;
-}
+const issueDeleted = (data) =>
+    `The issue is deleted in repository ${data.repository.name}\n` +
+    `Issue: ${data.issue.title}\n` +
+    `Description: ${data.issue.body}\n` +
+    `Author: ${data.issue.user.login}\n` +
+    `More details: ${data.issue.html_url}`;
 
 module.exports = formatNotification;
